c32c8eff4e407d76031409ff8089a36dc4f03199
Ilia Novoselov <ilya.novoselov@gmail.com>
2026-07-02 17:34:13 +0200

    Refactoring

--- flake.nix
+++ flake.nix
@@ -1,22 +1,60 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt rust-analyzer pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      treefmt-nix,
+    }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.rustPlatform.buildRustPackage {
+          name = "gardener";
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
         };
-      }
-    );
+      });
+
+      devShells = forAllSystems (
+        pkgs:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            rust-analyzer
+            pre-commit
+            rustPackages.clippy
+          ];
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        }
+      );
+
+      formatter = forAllSystems (
+        pkgs:
+        treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+          programs.keep-sorted.enable = true;
+        }
+      );
+
+      nixosModules.default = null;
+    };
 }
