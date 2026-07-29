d04e57b517df50f4b7e670ae83f6abc58859c0b5
Ilia Novoselov <ilya.novoselov@gmail.com>
2026-07-29 22:02:10 +0200

    Factored out tmpfiles module

--- src/untracked.rs
+++ src/untracked.rs
@@ -3,11 +3,9 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use systemd_tmpfiles::Directive;
-
 use crate::{
     config::{Config, OwnerModule},
-    declarative::{DeclaredFileType, DeclaredPathType, tree::Tree},
+    declarative::{DeclaredFileType, tmpfiles::add_systemd_tmpfiles, tree::Tree},
     fs::{FileType, Visitor, visit_dirs},
 };
 
@@ -258,51 +256,4 @@ impl<'a> Visitor<'a> for SimpleVisitor<'a> {
             false
         }
     }
-}
-
-fn add_systemd_tmpfiles(tree: &mut Tree) -> eyre::Result<()> {
-    let owner = OwnerModule::AdhocSystem {
-        name: "systemd-tmpfiles",
-    };
-    let output = std::process::Command::new("systemd-tmpfiles")
-        .arg("--cat-config")
-        .output()?;
-
-    // FIXME: return error
-    assert!(output.status.success());
-
-    let output = String::from_utf8(output.stdout)?;
-
-    let parsed = systemd_tmpfiles::parser::parse_str(&output)?;
-
-    for entry in parsed {
-        let maybe_entry_type = match entry.directive() {
-            Directive::CreateSymlink { .. } => {
-                Some(DeclaredPathType::File(DeclaredFileType::Symlink))
-            }
-            Directive::CreateFile { .. } | Directive::WriteToFile { .. } => {
-                Some(DeclaredPathType::File(DeclaredFileType::Regular))
-            }
-            Directive::CreateFifo { .. } => Some(DeclaredPathType::File(DeclaredFileType::Fifo)),
-            Directive::CreateCharDeviceNode { .. } => {
-                Some(DeclaredPathType::File(DeclaredFileType::CharDevice))
-            }
-            Directive::CreateBlockDeviceNode { .. } => {
-                Some(DeclaredPathType::File(DeclaredFileType::BlockDevice))
-            }
-            Directive::CreateDirectory { .. } | Directive::CreateSubvolume { .. } => {
-                Some(DeclaredPathType::OpenDirectory)
-            }
-            _ => None,
-        };
-
-        if let Some(entry_type) = maybe_entry_type {
-            // FIXME: return error
-            assert!(!entry.path_is_glob());
-
-            tree.add_path(owner, Path::new(entry.path()), entry_type)?;
-        }
-    }
-
-    Ok(())
 }
