f400833ac5bac001c0ecaa529b6208f6e01e43f1
Ilia Novoselov <ilya.novoselov@gmail.com>
2026-07-31 20:57:07 +0200

    Added flake-parts

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
@@ -10,52 +11,52 @@
   outputs =
     inputs@{
       self,
-      nixpkgs,
+      flake-parts,
       treefmt-nix,
+      ...
     }:
-    let
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-        ] (system: function nixpkgs.legacyPackages.${system});
-    in
-    {
-      packages = forAllSystems (pkgs: rec {
-        default = pkgs.rustPlatform.buildRustPackage {
-          name = "gardener";
-          src = ./.;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-          cargoLock.lockFile = ./Cargo.lock;
+      perSystem =
+        { pkgs, ... }:
+        {
+
+          packages = rec {
+            default = pkgs.rustPlatform.buildRustPackage {
+              name = "gardener";
+              src = ./.;
+
+              cargoLock.lockFile = ./Cargo.lock;
+            };
+            gardener = default;
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                cargo
+                rustc
+                rustfmt
+                rust-analyzer
+                pre-commit
+                rustPackages.clippy
+              ];
+              RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+            };
+          };
+
+          formatter = treefmt-nix.lib.mkWrapper pkgs {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.keep-sorted.enable = true;
+          };
         };
-        gardener = default;
-      });
 
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            rustc
-            rustfmt
-            rust-analyzer
-            pre-commit
-            rustPackages.clippy
-          ];
-          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
-        };
-      });
-
-      formatter = forAllSystems (
-        pkgs:
-        treefmt-nix.lib.mkWrapper pkgs {
-          projectRootFile = "flake.nix";
-          programs.nixfmt.enable = true;
-          programs.keep-sorted.enable = true;
-        }
-      );
-
-      nixosModules.default =
+      flake.nixosModules.default =
         { ... }:
         {
           imports = [
