9770fbccd2f25d7787baf89a65d13687b564f905
Ilia Novoselov <ilya.novoselov@gmail.com>
2026-07-31 21:20:10 +0200

    Applied formatting

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
   inputs = {
+    # keep-sorted start block=yes
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # keep-sorted end
   };
 
   outputs =
